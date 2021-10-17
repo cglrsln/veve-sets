46a0122b520891b74c866c1ff23faf1ffaf4b75f
Chris Mar <cmar@customink.com>
2021-10-17 10:11:15 -0400

    extract table into a component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
-//import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
+import SetTable from './SetTable';
 import { useState, useEffect} from 'react';
 
 import Container from 'react-bootstrap/Container';
-import Table from 'react-bootstrap/Table';
+
 
 function App() {
-  const [sets, setSets] = useState([])
-
-  useEffect(() => {
-    let path = `current.json`
-    axios.get(path)
-      .then(response => {
-        setSets(response.data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  })
-
-
-  sets.sort((a, b) => b.total - a.total)
-
-  const rows = sets.map((item, index) => {
-    return(
-      <tr key={index}>
-        <td>{item.name}</td>
-        <td>{item.season}</td>
-        <td>{item.collectibles.length}</td>
-        <td>${item.total.toFixed(2)}</td>
-      </tr>
-    )
-  })
-
   return (
     <Container className="p-3">
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Set</th>
-            <th>Season</th>
-            <th># of Collectibles</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </Table>
+      <SetTable/>
     </Container>
   )
 }
---
+++ src/SetTable.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import { useState, useEffect} from 'react';
+import Table from 'react-bootstrap/Table';
+
+
+function SetTable() {
+ const [sets, setSets] = useState([])
+
+  useEffect(() => {
+    let path = `https://cmar.github.io/veve-price-scraper/current.json`
+    axios.get(path)
+      .then(response => {
+        setSets(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  })
+
+ //sets.sort((a, b) => b.total - a.total)
+
+  sets.sort((a, b) => {
+    if (a.season < b.season) { return -1 }
+    if (a.season > b.season) { return 1 }
+    if (a.name < b.name) { return -1 }
+    if (a.name > b.name) { return 1 }
+    return b.total - a.total
+  })
+
+  const rows = sets.map((item, index) => {
+    return(
+      <tr key={index}>
+        <td>{item.name}</td>
+        <td>{item.season}</td>
+        <td>{item.collectibles.length}</td>
+        <td>${item.total.toFixed(2)}</td>
+      </tr>
+    )
+  })
+
+  return(
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Set</th>
+          <th>Season</th>
+          <th># of Collectibles</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </Table>
+  )
+}
+
+export default SetTable
