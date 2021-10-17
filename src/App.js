78d4292481cc6b2d5912d750e366039df6983dfa
Chris Mar <cmar@customink.com>
2021-10-17 11:39:14 -0400

    sortable table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import SetTable from './SetTable';
+
 import { useState, useEffect} from 'react';
 
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 
 function App() {
   return (
-    <Container className="p-3">
+    <Container>
+      <Navbar bg="light" className="mb-3">
+        <Container>
+          <Navbar.Brand href="#home">VeVe Sets</Navbar.Brand>
+        </Container>
+      </Navbar>
       <SetTable/>
     </Container>
   )
--- src/SetTable.js
+++ src/SetTable.js
@@ -3,9 +3,11 @@ import { useState, useEffect} from 'react';
 import Table from 'react-bootstrap/Table';
 
 function SetTable() {
- const [sets, setSets] = useState([])
+  const [sets, setSets] = useState([])
+  const [sortOrder, setSortOrder] = useState('')
 
   useEffect(() => {
+    console.log("fetching data")
     let path = `https://cmar.github.io/veve-price-scraper/current.json`
     axios.get(path)
       .then(response => {
@@ -14,15 +16,15 @@ function SetTable() {
       .catch(error => {
         console.log(error)
       })
-  })
+  }, [])
 
-  sets.sort((a, b) => {
-    if (a.season < b.season) { return -1 }
-    if (a.season > b.season) { return 1 }
-    if (a.name < b.name) { return -1 }
-    if (a.name > b.name) { return 1 }
-    return b.total - a.total
-  })
+  // sets.sort((a, b) => {
+  //   if (a.season < b.season) { return -1 }
+  //   if (a.season > b.season) { return 1 }
+  //   if (a.name < b.name) { return -1 }
+  //   if (a.name > b.name) { return 1 }
+  //   return b.total - a.total
+  // })
 
   const rows = sets.map((item, index) => {
     const formattedTotal = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
@@ -30,22 +32,52 @@ function SetTable() {
 
     return(
       <tr key={index}>
-        <td>{item.name}</td>
         <td>{item.season}</td>
+        <td>{item.name}</td>
         <td>{item.collectibles.length}</td>
         <td>{formattedTotal}</td>
       </tr>
     )
   })
 
+  function sortName() {
+    let order = (sortOrder === 'name_asc') ? 'name_desc' : 'name_asc'
+    let sortedSet = [...sets]
+
+    if (order === 'name_asc') {
+      sortedSet = sortedSet.sort((a, b) => a.name.localeCompare(b.name))
+    }
+    else if (order === 'name_desc') {
+      sortedSet = sortedSet.sort((a, b) => b.name.localeCompare(a.name))
+    }
+
+    setSortOrder(order)
+    setSets(sortedSet)
+  }
+
+  function sortPrice() {
+    let order = (sortOrder === 'price_asc') ? 'price_desc' : 'price_asc'
+    let sortedSet = [...sets]
+
+    if (order === 'price_asc') {
+      sortedSet = sortedSet.sort((a, b) => a.total - b.total)
+    }
+    else if (order === 'price_desc') {
+      sortedSet = sortedSet.sort( (a, b) => b.total - a.total)
+    }
+
+    setSortOrder(order)
+    setSets(sortedSet)
+  }
+
   return(
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>Set</th>
           <th>Season</th>
+          <th onClick={sortName}>Set</th>
           <th># of Collectibles</th>
-          <th>Price</th>
+          <th onClick={sortPrice}>Price</th>
         </tr>
       </thead>
       <tbody>
