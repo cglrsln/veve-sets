da3c376f5190bb4046bc801c0abe99abcca195c3
Chris Mar <cmar@customink.com>
2021-10-17 07:21:47 -0400

    fetch sets and display in table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect} from 'react';
 
 function App() {
+  const [sets, setSets] = useState([])
+
+  useEffect(() => {
+    let path = `current.json`
+    axios.get(path)
+      .then(response => {
+        setSets(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  })
+
+  const rows = sets.map((item, index) => {
+    return(
+      <tr key={index}>
+        <td>{item.name}</td>
+        <td>{item.season}</td>
+        <td>{item.collectibles.length}</td>
+        <td>${item.total.toFixed(2)}</td>
+      </tr>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+        {rows}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
